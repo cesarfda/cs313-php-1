ac04ec716bf654a23f4e7d775d6e898cb53ddc22
Cesar <ara17008@byui.edu>
2020-10-24 16:49:11 -0600

    small change dir

--- web/idahocamp/index.php
+++ web/idahocamp/index.php
@@ -52,9 +52,9 @@ switch ($action){
         include './views/campgrounds/new.php';
         exit;
         }
-
+        
         //Validate Image
-        $target_dir = "images/";
+        $target_dir = "./images";
         $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
         $uploadOk = 1;
         $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
