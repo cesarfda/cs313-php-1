4a8e0ad15dabfab01032906dba7ea7a72be86d34
Cesar <ara17008@byui.edu>
2020-10-24 16:57:10 -0600

    changed image validation

--- web/idahocamp/index.php
+++ web/idahocamp/index.php
@@ -42,7 +42,6 @@ switch ($action){
     case 'addNewCamp':
         //Fetch Data
         $campName = filter_input(INPUT_POST, 'campName', FILTER_SANITIZE_STRING);
-        $campImage = filter_input(INPUT_POST, 'campImage', FILTER_SANITIZE_STRING);
         $campDescription = filter_input(INPUT_POST, 'campDescription', FILTER_SANITIZE_STRING);
         $campLocation = filter_input(INPUT_POST, 'campLocation', FILTER_SANITIZE_STRING);
         $author = 1;
@@ -53,56 +52,34 @@ switch ($action){
         exit;
         }
         
-        //Validate Image
-        $target_dir = "./images";
-        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-        $uploadOk = 1;
-        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-        // Check if image file is a actual image or fake image
-        if(isset($_POST["submit"])) {
-            $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-            if($check !== false) {
-            echo "File is an image - " . $check["mime"] . ".";
-            $uploadOk = 1;
-            } else {
-            echo "File is not an image.";
-            $uploadOk = 0;
+        if(isset($_FILES['image'])){
+            $errors= array();
+            $file_name = $_FILES['image']['name'];
+            $file_size =$_FILES['image']['size'];
+            $file_tmp =$_FILES['image']['tmp_name'];
+            $file_type=$_FILES['image']['type'];
+            $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+            
+            $extensions= array("jpeg","jpg","png");
+            
+            if(in_array($file_ext,$extensions)=== false){
+               $errors[]="extension not allowed, please choose a JPEG or PNG file.";
             }
-        }
-        
-        // Check if file already exists
-        if (file_exists($target_file)) {
-            echo "<p>Sorry, file already exists.</p>";
-            $uploadOk = 0;
-        }
-        
-        // Check file size
-        if ($_FILES["fileToUpload"]["size"] > 500000) {
-            echo "<p>Sorry, your file is too large.</p>";
-            $uploadOk = 0;
-        }
-        
-        // Allow certain file formats
-        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-        && $imageFileType != "gif" ) {
-            echo "<p>Sorry, only JPG, JPEG, PNG & GIF files are allowed.</p>";
-            $uploadOk = 0;
-        }
-        
-        // Check if $uploadOk is set to 0 by an error
-        if ($uploadOk == 0) {
-            echo "Sorry, your file was not uploaded.";
-        // if everything is ok, try to upload file
-        } else {
-            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-            echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
-            } else {
-            echo "Sorry, there was an error uploading your file.";
+            
+            if($file_size > 2097152){
+               $errors[]='File size must be excately 2 MB';
             }
-        }
-        
+            
+            if(empty($errors)==true){
+               move_uploaded_file($file_tmp,"images/".$file_name);
+               echo "Success";
+            }else{
+               print_r($errors);
+            }
+         }
+        $campImage = "images/".$file_name;
         //Send data to model
-        $vehicleOutcome = addNewCamp($campName,$target_file,$campDescription, $campLocation, $author);
+        $vehicleOutcome = addNewCamp($campName,$campImage,$campDescription, $campLocation, $author);
         // Check and report the result
         if($vehicleOutcome === 1){
         $message = "<p>Camp added successfully.</p>";
