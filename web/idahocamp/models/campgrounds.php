43c1fbc5de304817f041b1ad02d01e3e23b6123c
Cesar <ara17008@byui.edu>
2020-10-24 17:36:53 -0600

    updated fucntion

--- web/idahocamp/models/campgrounds.php
+++ web/idahocamp/models/campgrounds.php
@@ -2,23 +2,5 @@
 //===========================
 //FUNCTION TO ADD A VEHICLE
 //===========================
-  function addNewCamp($campName,$campImage,$campDescription, $campLocation, $author){
-    // Create a connection object from the phpmotors connection function
-    $db = get_db(); 
-    // The SQL statement to be used with the database 
-    $sql = 'INSERT INTO camp_site (name, image, description, location, creation_date, author) 
-    VALUES (:campName, :campImage, :campDescription, :campLocation, current_timestamp, :author)'; 
-    // The next line creates the prepared statement using the phpmotors connection      
-    $stmt = $db->prepare($sql);
-    // The next four lines replace the placeholders in the SQL
-    // statement with the actual values in the variables
-    // and tells the database the type of data it is
-    $stmt->bindValue(':campName', $campName, PDO::PARAM_STR);
-    $stmt->bindValue(':campImage', $campImage, PDO::PARAM_STR);
-    $stmt->bindValue(':campDescription', $campDescription, PDO::PARAM_STR);
-    $stmt->bindValue(':campLocation', $campLocation, PDO::PARAM_STR);
-    $stmt->bindValue(':author', $author, PDO::PARAM_INT);
-    // The next line runs the prepared statement 
-    $stmt->execute(); 
-  }
+  
   ?>
