0f4e4c147a77762e1064fb53dc4d90e20c4ddfc4
Cesar <ara17008@byui.edu>
2020-10-24 15:14:37 -0600

    added main controller

---
+++ web/idahocamp/index.php
@@ -0,0 +1,26 @@
+<?php
+// MAIN CONTROLER
+
+// Create or access a Session
+session_start();
+
+// Check if the firstname cookie exists, get its value
+if(isset($_COOKIE['firstname'])){
+    $cookieFirstname = filter_input(INPUT_COOKIE, 'firstname', FILTER_SANITIZE_STRING);
+}
+
+$action = filter_input(INPUT_POST, 'action');
+if ($action == NULL){
+ $action = filter_input(INPUT_GET, 'action');
+}
+
+switch ($action){
+    case 'template':
+        include 'view/template.php';
+     break;
+    
+    default:
+     include 'views/campgrounds/index.php';
+}
+
+?>
