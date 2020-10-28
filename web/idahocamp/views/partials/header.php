aa4c54b2cc112aa2a6093190c67ac5fc5792305a
Cesar <ara17008@byui.edu>
2020-10-28 13:20:37 -0600

    fixed session issue

--- web/idahocamp/views/partials/header.php
+++ web/idahocamp/views/partials/header.php
@@ -43,7 +43,7 @@
             </li>
 
               <?php 
-              if($_SESSION['loggedin']){?>
+              if(isset($_SESSION['loggedin']) && $_SESSION['loggedin']){?>
               <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
                 aria-expanded="false">
@@ -52,7 +52,7 @@
               </a>
               <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                 <a class='dropdown-item' href='./accounts'>Profile</a>
-                <a class="dropdown-item" href=".accounts/index.php?action=logout">Log Out</a>
+                <a class="dropdown-item" href="./accounts/index.php?action=logout">Log Out</a>
               </div>
               </li>
               <?php } ?>
