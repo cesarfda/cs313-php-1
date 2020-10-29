e4a10f25aad198a763adff8bd68f2e99a18ad239
Cesar <ara17008@byui.edu>
2020-10-28 21:58:15 -0600

    added css for user details

--- web/idahocamp/views/users/show.php
+++ web/idahocamp/views/users/show.php
@@ -5,14 +5,14 @@
 ?>
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/idahocamp/views/partials/header.php'; ?>
   <div class="container mt-4">    
-    <div class="row">
+    <div class="row justify-content-center">
         <div class="card shadow-sm mt-3 mb-3">
           <div class="card-header text-capitalize">
-            <?php if(isset($_SESSION['clientData']['full_name'])){echo "value={$_SESSION['clientData']['full_name']}";} ?>            
+            <?php if(isset($_SESSION['clientData']['full_name'])){echo "{$_SESSION['clientData']['full_name']}";} ?>            
           </div>
           <ul class="list-group list-group-flush">
               <li class="list-group-item">
-                <i class="fas fa-envelope"></i> <strong>Email: </strong><a href="mailto:<%= user.email %> "><?php if(isset($_SESSION['clientData']['email'])){echo "value={$_SESSION['clientData']['email']}";}?></a>             
+                <i class="fas fa-envelope"></i> <strong>Email: </strong><?php if(isset($_SESSION['clientData']['email'])){echo "{$_SESSION['clientData']['email']}";}?></a>             
               </li>                            
                 <li class="list-group-item d-flex justify-content-center">
                   <a class="btn btn-warning btn-sm float-left mr-1" href="/idahocamp/accounts/index.php?action=updateInfo">Edit Profile</a>
