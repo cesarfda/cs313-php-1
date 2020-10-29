23700c2981a9f40756a93bf0b06abae0ff18bf49
Cesar <ara17008@byui.edu>
2020-10-28 21:11:20 -0600

    fixed div

--- web/idahocamp/views/campgrounds/show.php
+++ web/idahocamp/views/campgrounds/show.php
@@ -1,8 +1,8 @@
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/idahocamp/views/partials/header.php'; ?>
 
-    <div id="campgroundsContainer" class="container mt-4">
-      <!-- <div class="row">
-        <div class="col-md-3">
+<div id="campgroundsContainer" class="container mt-4">
+  <div class="row">
+    <!--<div class="col-md-3">
           <div class="d-none d-md-block">
             <div class="lead text-center text-capitalize">
               <?php# if(isset($campInfo['name'])){echo "$campInfo[name]";} ?>
@@ -17,44 +17,45 @@
                   </div>
           </div>
         </div> -->
-        <div class="col-md-9">
-          <div class="card shadow-sm">
-            <img class="card-img-top" src="<?php if(isset($campInfo['image'])){echo "$campInfo[image]";} ?>">
-            <div class="card-body">
-              <h5 class="card-title text-capitalize">
-              <?php if(isset($campInfo['name'])){echo "$campInfo[name]";} ?>
-            </h5>
-              <p class="card-text">
-                <?php if(isset($campInfo['description'])){echo "$campInfo[description]";} ?>
-                  <div class="d-block d-sm-none d-none d-sm-block d-md-none">
-                    <hr>
-                    <h5 class="card-title"><i class="fas fa-map-marker-alt"></i> Location</h5>
-                    <?php if(isset($campInfo['location'])){echo "$campInfo[location]";} ?>
-                  </div>
-              </p>
+    <div class="col-md-9">
+      <div class="card shadow-sm">
+        <img class="card-img-top" src="<?php if(isset($campInfo['image'])){echo "$campInfo[image]";} ?>">
+        <div class="card-body">
+          <h5 class="card-title text-capitalize">
+            <?php if(isset($campInfo['name'])){echo "$campInfo[name]";} ?>
+          </h5>
+          <p class="card-text">
+            <?php if(isset($campInfo['description'])){echo "$campInfo[description]";} ?>
+            <div class="d-block d-sm-none d-none d-sm-block d-md-none">
               <hr>
-              <p class="card-text text-muted">
-                <span>Submitted by
-                <?php if(isset($authorName['full_name'])){echo "$authorName[full_name]";} ?></a> on
-                <?php if(isset($campInfo['creation_date'])){echo date_format(date_create($campInfo['creation_date']), 'd/m/Y');} ?>
-              </span>
-              </p>
-              <!-- Add session to control delete buttons -->
-                <form id="deleteForm" action="./index.php" method="GET" class="float-right">
-                  <button type="submit" class="delBtn btn text-dark btn-lg">
-                  <input type="hidden" name="action" value="del">
-                  <input type="hidden" name="id" value="<?php if(isset($campId)){echo $campId;} elseif(isset($campInfo['id'])){ echo $campInfo['id'];}?>">
-                  <i class="fas fa-trash-alt"></i>
-                </button>
-                </form>
-                <a href="./index.php?action=mod&id=<?= $campInfo['id'] ?>" class="btn text-dark btn-lg float-right">
-                <i class="fas fa-pencil-alt"></i>
-              </a>
+              <h5 class="card-title"><i class="fas fa-map-marker-alt"></i> Location</h5>
+              <?php if(isset($campInfo['location'])){echo "$campInfo[location]";} ?>
             </div>
+          </p>
+          <hr>
+          <p class="card-text text-muted">
+            <span>Submitted by
+              <?php if(isset($authorName['full_name'])){echo "$authorName[full_name]";} ?></a> on
+              <?php if(isset($campInfo['creation_date'])){echo date_format(date_create($campInfo['creation_date']), 'd/m/Y');} ?>
+            </span>
+          </p>
+          <!-- Add session to control delete buttons -->
+          <form id="deleteForm" action="./index.php" method="GET" class="float-right">
+            <button type="submit" class="delBtn btn text-dark btn-lg">
+              <input type="hidden" name="action" value="del">
+              <input type="hidden" name="id"
+                value="<?php if(isset($campId)){echo $campId;} elseif(isset($campInfo['id'])){ echo $campInfo['id'];}?>">
+              <i class="fas fa-trash-alt"></i>
+            </button>
+          </form>
+          <a href="./index.php?action=mod&id=<?= $campInfo['id'] ?>" class="btn text-dark btn-lg float-right">
+            <i class="fas fa-pencil-alt"></i>
+          </a>
         </div>
       </div>
-                                      </div>
+    </div>
+  </div>
 
-    <footer>
-            <?php include $_SERVER['DOCUMENT_ROOT'] . '/idahocamp/views/partials/footer.php'; ?>
-    </footer>
+  <footer>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/idahocamp/views/partials/footer.php'; ?>
+  </footer>
