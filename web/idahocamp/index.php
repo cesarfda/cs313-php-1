ce6558c7e784b12f9e7bd01fba58326d5ae741db
Cesar <ara17008@byui.edu>
2020-10-28 20:33:42 -0600

    updated models

--- web/idahocamp/index.php
+++ web/idahocamp/index.php
@@ -1,26 +1,11 @@
 <?php
 // MAIN CONTROLER
-require_once './library/connections.php';
 require_once './models/accounts.php';
-$db = get_db(); 
+require_once './models/campgrounds.php';
+
+#$db = get_db(); 
 // Create or access a Session
 session_start();
-
-function getCampInfo($campId){
-    $db = get_db();
-    $sql = 'SELECT * FROM camp_site WHERE id = :campId';
-    $stmt = $db->prepare($sql);
-    $stmt->bindValue(':campId', $campId, PDO::PARAM_INT);
-    $stmt->execute();
-    $invInfo = $stmt->fetch(PDO::FETCH_ASSOC);
-    $stmt->closeCursor();
-  return $invInfo;
-  }
-
-// Check if the firstname cookie exists, get its value
-if(isset($_COOKIE['firstname'])){
-    $cookieFirstname = filter_input(INPUT_COOKIE, 'firstname', FILTER_SANITIZE_STRING);
-}
 
 $action = filter_input(INPUT_POST, 'action');
 if ($action == NULL){
@@ -57,25 +42,7 @@ switch ($action){
          #$campImage = "images/".$file_name;
          //Send data to model
          try{
-            
-                // Create a connection object from the phpmotors connection function
-                $db = get_db(); 
-                // The SQL statement to be used with the database 
-                $sql = 'INSERT INTO camp_site (name, image, description, location, creation_date, author) 
-                VALUES (:campName, :campImage, :campDescription, :campLocation, current_timestamp, :author)'; 
-                // The next line creates the prepared statement using the phpmotors connection      
-                $stmt = $db->prepare($sql);
-                // The next four lines replace the placeholders in the SQL
-                // statement with the actual values in the variables
-                // and tells the database the type of data it is
-                $stmt->bindValue(':campName', $campName, PDO::PARAM_STR);
-                $stmt->bindValue(':campImage', $campImage, PDO::PARAM_STR);
-                $stmt->bindValue(':campDescription', $campDescription, PDO::PARAM_STR);
-                $stmt->bindValue(':campLocation', $campLocation, PDO::PARAM_STR);
-                $stmt->bindValue(':author', $author, PDO::PARAM_INT);
-                // The next line runs the prepared statement 
-                $stmt->execute(); 
-              
+            createCamp($campName, $campImage, $campDescription, $campLocation, $author);
          }
          catch(Exception $ex){
              echo"Error with DB. Details:$ex";
@@ -120,19 +87,7 @@ switch ($action){
             }
              //Send data to model
              try{
-                    // Create a connection object from the phpmotors connection function
-                    $db = get_db(); 
-                    // The SQL statement to be used with the database 
-                    $sql = 'DELETE FROM camp_site WHERE id = :campId'; 
-                    // The next line creates the prepared statement using the phpmotors connection      
-                    $stmt = $db->prepare($sql);
-                    // The next four lines replace the placeholders in the SQL
-                    // statement with the actual values in the variables
-                    // and tells the database the type of data it is
-                    $stmt->bindValue(':campId', $campId, PDO::PARAM_INT);
-                    // The next line runs the prepared statement 
-                    $stmt->execute();
-                    $stmt->closeCursor();  
+                deleteCamp($campId);
              }
              catch(Exception $ex){
                  echo"Error with DB. Details:$ex";
@@ -156,27 +111,9 @@ switch ($action){
                 include './views/campgrounds/edit.php';
                 exit;
             }
-            #var_dump($campDescription) ;
-             #$campImage = "images/".$file_name;
              //Send data to model
              try{
-                    // Create a connection object from the phpmotors connection function
-                    $db = get_db(); 
-                    // The SQL statement to be used with the database 
-                    $sql = 'UPDATE camp_site SET name = :campName, image = :campImage, description = :campDescription, location = :campLocation WHERE id = :campId'; 
-                    // The next line creates the prepared statement using the phpmotors connection      
-                    $stmt = $db->prepare($sql);
-                    // The next four lines replace the placeholders in the SQL
-                    // statement with the actual values in the variables
-                    // and tells the database the type of data it is
-                    $stmt->bindValue(':campName', $campName, PDO::PARAM_STR);
-                    $stmt->bindValue(':campImage', $campImage, PDO::PARAM_STR);
-                    $stmt->bindValue(':campDescription', $campDescription, PDO::PARAM_STR);
-                    $stmt->bindValue(':campLocation', $campLocation, PDO::PARAM_STR);
-                    $stmt->bindValue(':campId', $campId, PDO::PARAM_INT);
-                    // The next line runs the prepared statement 
-                    $stmt->execute();
-                    $stmt->closeCursor();  
+                updateCamp($campId, $campLocation, $campDescription, $campImage, $campName);
              }
              catch(Exception $ex){
                  echo"Error with DB. Details:$ex";
--- web/idahocamp/models/campgrounds.php
+++ web/idahocamp/models/campgrounds.php
@@ -2,5 +2,72 @@
 //===========================
 //FUNCTION TO ADD A Campsite
 //===========================
-  
+require_once './library/connections.php';
+
+function getCampInfo($campId){
+  $db = get_db();
+  $sql = 'SELECT * FROM camp_site WHERE id = :campId';
+  $stmt = $db->prepare($sql);
+  $stmt->bindValue(':campId', $campId, PDO::PARAM_INT);
+  $stmt->execute();
+  $invInfo = $stmt->fetch(PDO::FETCH_ASSOC);
+  $stmt->closeCursor();
+return $invInfo;
+}
+
+function createCamp($campName, $campImage, $campDescription, $campLocation, $author){
+  // Create a connection object from the phpmotors connection function
+  $db = get_db(); 
+  // The SQL statement to be used with the database 
+  $sql = 'INSERT INTO camp_site (name, image, description, location, creation_date, author) 
+  VALUES (:campName, :campImage, :campDescription, :campLocation, current_timestamp, :author)'; 
+  // The next line creates the prepared statement using the phpmotors connection      
+  $stmt = $db->prepare($sql);
+  // The next four lines replace the placeholders in the SQL
+  // statement with the actual values in the variables
+  // and tells the database the type of data it is
+  $stmt->bindValue(':campName', $campName, PDO::PARAM_STR);
+  $stmt->bindValue(':campImage', $campImage, PDO::PARAM_STR);
+  $stmt->bindValue(':campDescription', $campDescription, PDO::PARAM_STR);
+  $stmt->bindValue(':campLocation', $campLocation, PDO::PARAM_STR);
+  $stmt->bindValue(':author', $author, PDO::PARAM_INT);
+  // The next line runs the prepared statement 
+  $stmt->execute(); 
+}
+
+function updateCamp($campId, $campLocation, $campDescription, $campImage, $campName){
+  // Create a connection object from the phpmotors connection function
+  $db = get_db(); 
+  // The SQL statement to be used with the database 
+  $sql = 'UPDATE camp_site SET name = :campName, image = :campImage, description = :campDescription, location = :campLocation WHERE id = :campId'; 
+  // The next line creates the prepared statement using the phpmotors connection      
+  $stmt = $db->prepare($sql);
+  // The next four lines replace the placeholders in the SQL
+  // statement with the actual values in the variables
+  // and tells the database the type of data it is
+  $stmt->bindValue(':campName', $campName, PDO::PARAM_STR);
+  $stmt->bindValue(':campImage', $campImage, PDO::PARAM_STR);
+  $stmt->bindValue(':campDescription', $campDescription, PDO::PARAM_STR);
+  $stmt->bindValue(':campLocation', $campLocation, PDO::PARAM_STR);
+  $stmt->bindValue(':campId', $campId, PDO::PARAM_INT);
+  // The next line runs the prepared statement 
+  $stmt->execute();
+  $stmt->closeCursor();  
+}
+
+function deleteCamp($campId){
+  // Create a connection object from the phpmotors connection function
+  $db = get_db(); 
+  // The SQL statement to be used with the database 
+  $sql = 'DELETE FROM camp_site WHERE id = :campId'; 
+  // The next line creates the prepared statement using the phpmotors connection      
+  $stmt = $db->prepare($sql);
+  // The next four lines replace the placeholders in the SQL
+  // statement with the actual values in the variables
+  // and tells the database the type of data it is
+  $stmt->bindValue(':campId', $campId, PDO::PARAM_INT);
+  // The next line runs the prepared statement 
+  $stmt->execute();
+  $stmt->closeCursor();  
+}
   ?>
